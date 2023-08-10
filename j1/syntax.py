06e757035f04187130f4bfbe03e6025189ee9389
Shayan-02 <upm200@gmail.com>
2023-08-11 02:09:51 +0330

    j1

---
+++ j1/syntax.py
@@ -0,0 +1,20 @@
+# + . - . * . / . % . ** . // . = . == . != . %= ...
+print('hi')
+print(1)
+a = 3
+b = 2
+# result = a ** b
+# result2 = 10 // 3
+e = "ali"
+# f = 3 * e
+
+x = input("enter a number: ")
+# print(type(x))
+
+# print(f"result: {result}")
+# print("result2: {}".format(result2))
+
+# a //= b
+
+y = "amir"
+z = "mohammadi"
